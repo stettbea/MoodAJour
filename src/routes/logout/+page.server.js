eea83dfbbdbe89b21f2db66afc23f809ab9395eb
stettbea <stettler.beatrice>
2026-05-08 18:57:34 +0200

    Korrektur Logout

--- src/routes/logout/+page.server.js
+++ src/routes/logout/+page.server.js
@@ -3,6 +3,6 @@ import { redirect } from '@sveltejs/kit';
 export const actions = {
 	default: async ({ cookies }) => {
 		cookies.delete('userId', { path: '/' });
-		redirect(303, '/login');
+		throw redirect(303, '/login');
 	}
 };
