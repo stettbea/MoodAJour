caa09fb0a7c3b5084054b5f658f8ccc5eb29ad5a
stettbea <stettler.beatrice>
2026-05-08 19:02:21 +0200

    Korrektur Logout Homepage 2

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,4 +1,9 @@
-export function load({ locals }) {
+import { redirect } from '@sveltejs/kit';
+
+export function load({ locals, url }) {
+	if (!locals.user && url.pathname !== '/login' && url.pathname !== '/tipps') {
+		throw redirect(303, '/login');
+	}
 	return {
 		user: locals.user
 	};
